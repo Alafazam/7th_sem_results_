9c9e357c61a53129a1a2e036288cd62fe9165554
Alaf Azam <alafazam@gmail.com>
2016-02-26 01:43:19 +0530

    added results

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,18 @@ import os
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
+import json
+
+
+
+
+input_file_name = 'results.json'
+openFile =  open(input_file_name, 'r')
+inputFile = json.load(openFile)
+
+subject_wise_results = inputFile['subject_wise_results'] 
+student_wise_results = inputFile['student_wise_results']
+
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
@@ -10,11 +22,11 @@ app.config.from_pyfile('flaskapp.cfg')
 def index():
     return render_template('index.html')
 
-@app.route('/<path:resource>')
-def serveStaticResource(resource):
-    return send_from_directory('static/', resource)
+# @app.route('/<path:resource>')
+# def serveStaticResource(resource):
+#     return send_from_directory('static/', resource)
 
-@app.route("/test")
+@app.route("/IT")
 def test():
     return "<strong>It's Alive!</strong>"
 
