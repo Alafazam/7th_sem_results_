0ac927280696d4cf3ce5ad1be2a893d8952d814c
Alaf Azam <alafazam@gmail.com>
2016-02-27 03:08:41 +0530

    add subject list

--- flaskapp.py
+++ flaskapp.py
@@ -12,6 +12,7 @@ openFile =  open(input_file_name, 'r')
 inputFile = json.load(openFile)
 
 subject_wise_results = inputFile['subject_wise_results'] 
+
 student_wise_results = inputFile['student_wise_results']
 
 
@@ -22,13 +23,19 @@ app.config.from_pyfile('flaskapp.cfg')
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    print student_wise_results
+    return render_template('temp.html',student_wise_results=student_wise_results)
+
+
 # @app.route('/<path:resource>')
 # def serveStaticResource(resource):
 #     return send_from_directory('static/', resource)
 
-@app.route("/IT")
-def test():
-    return "<strong>It's Alive!</strong>"
+# @app.route("/IT")
+# def test():
+#     return "<strong>It's Alive!</strong>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=Trues)
